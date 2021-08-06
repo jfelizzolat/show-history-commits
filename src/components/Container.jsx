847efb3cf78c70681cec712ef807e5ed86d0c42e
Jeffrey Felizzola Tapia <jfelizzolat@gmail.com>
2021-08-06 11:37:50 -0300

    Estructura de los datos del usuario en el header

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import Header from "./Header";
 
 const Container = () => {
+  const data_user = {
+    avatar:
+      "https://i1.wp.com/www.culturaldevoto.com.ar/wp-content/plugins/userswp/assets/images/no_profile.png?ssl=1",
+    name: "Jeffrey Felizzola Tapia",
+    username: "jfelizzolat",
+    email: "jfelizzolat@gmail.com ",
+    website: "https://jfelizzolat.dev",
+  };
   return (
     <>
-      <Header />
+      <Header data={data_user} />
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "../assets/sass/Header.scss";
 
-const Header = () => {
-  return <div className="header"></div>;
+const Header = ({ data }) => {
+  const { avatar, name, username, email, website } = data;
+  return (
+    <div className="header">
+      <div className="header__content">
+        <div className="header__data">
+          <div className="avatar">
+            <img
+              src={avatar}
+              alt={name}
+              className="avatar__image avatar--large"
+            />
+          </div>
+          <div className="header__info">
+            <div className="text">
+              <label className="text__label">Nombre</label>
+              <span className="text__user">{name}</span>
+            </div>
+            <div className="text">
+              <label className="text__label">Usuario Github</label>
+              <span className="text__user">{username}</span>
+            </div>
+            <div className="text">
+              <label className="text__label">Email</label>
+              <span className="text__user">{email}</span>
+            </div>
+            <div className="text">
+              <label className="text__label">Sitio web</label>
+              <a
+                href={website}
+                target="_blank"
+                rel="noreferrer"
+                className="text__link"
+              >
+                <span className="text__user">{website}</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="header__actions"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
